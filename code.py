4ea8639d1ccce7c7280387cb357174e1c28764ab
Mia-g45 <maya.garnier@gmail.com>
2025-03-30 14:30:20 +0200

    j'ai fini la première image, avec le cadre et le titre

--- code.py
+++ code.py
@@ -1,25 +1,32 @@
 import pyxel
 
+
+
 class App:
     def __init__(self):
-        pyxel.init(240, 140, title="Classe App", fps=60)
+        pyxel.init(240, 140, title="phantom castle", fps=60)
         pyxel.load("images.pyxres")
+        self.temps_titre = 100 #temps d'affichage du titre
         pyxel.run(self.update, self.draw)
         pyxel.show() #si on appuie sur la touche echap la fenêtre se ferme
 
 
-
     def update(self):
-        #pyxel.mouse(True)
-        pass
+        if pyxel.frame_count == self.temps_titre: #nettoyer l'écran aprés le titre
+            pyxel.cls(0)
+        if pyxel.frame_count == 100: #apparition de la souris au bout d'un certain temps
+            self.apparition_souris()
+
 
 
     def draw(self):
-        #pyxel.cls(0)
-        self.cadre_intro()
+        pyxel.cls(0)
+        if pyxel.frame_count < self.temps_titre:
+            self.cadre()
+            self.titre()
 
-    def cadre_intro(self):
-        """La fonction dessine le cadre de l'intro et y met le textedu debut de l'histoire."""
+    def cadre(self):
+        """La fonction dessine le cadre."""
         for x in range(pyxel.width):
             for y in range(pyxel.height):
                 if x <= 2 or x >= pyxel.width-3 or y <= 2 or y >= pyxel.height-3:
@@ -29,19 +36,42 @@ class App:
         pyxel.blt(222, 122, 0, 0, 0, -16, 16, 0) #coin bas droit
         pyxel.blt(222, 2, 0, 0, 0, -16, -16, 0) #coin haut droit
 
-        pyxel.blt(15, 35, 0, 16, 0, 6, 8, 0) #P
-        pyxel.blt(25, 35, 0, 24, 0, 8, 8, 0) #H
-        pyxel.blt(35, 35, 0, 32, 0, 8, 8, 0) #A
-        pyxel.blt(45, 35, 0, 40, 0, 8, 8, 0) #N
-        pyxel.blt(55, 35, 0, 48, 0, 7, 8, 0) #T
-        pyxel.blt(65, 35, 0, 55, 0, 6, 8, 0) #O
-        pyxel.blt(75, 35, 0, 16, 8, 9, 8, 0) #M
-        pyxel.blt(85, 35, 0, 44, 8, 6, 8, 0) #E
+    def titre(self):
+        """écrit le titre du projet"""
+        pyxel.blt(91, 57, 0, 16, 0, 6, 8, 0) #P
+        pyxel.blt(98, 57, 0, 24, 0, 8, 8, 0) #H
+        pyxel.blt(107, 57, 0, 32, 0, 8, 8, 0) #A
+        pyxel.blt(116, 57, 0, 40, 0, 8, 8, 0) #N
+        pyxel.blt(125, 57, 0, 48, 0, 7, 8, 0) #T
+        pyxel.blt(133, 57, 0, 55, 0, 6, 8, 0) #O
+        pyxel.blt(140, 57, 0, 16, 8, 9, 8, 0) #M
 
-        pyxel.blt(15, 45, 0, 25, 8, 6, 8, 0) #C
-        pyxel.blt(25, 45, 0, 32, 0, 8, 8, 0) #A
-        pyxel.blt(35, 45, 0, 32, 8, 6, 8, 0) #S
-        pyxel.blt(45, 45, 0, 48, 0, 7, 8, 0) #T
-        pyxel.blt(55, 45, 0, 38, 8, 6, 8, 0) #L
-        pyxel.blt(65, 45, 0, 44, 8, 6, 8, 0) #E
+        pyxel.blt(98, 67, 0, 25, 8, 6, 8, 0) #C
+        pyxel.blt(105, 67, 0, 32, 0, 8, 8, 0) #A
+        pyxel.blt(114, 67, 0, 32, 8, 6, 8, 0) #S
+        pyxel.blt(121, 67, 0, 48, 0, 7, 8, 0) #T
+        pyxel.blt(129, 67, 0, 38, 8, 6, 8, 0) #L
+        pyxel.blt(136, 67, 0, 44, 8, 6, 8, 0) #E
+
+    def apparition_souris(self):
+        pyxel.mouse(True)
 App()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
